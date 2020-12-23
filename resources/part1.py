71f8e13997f3aa65308f2296498660fb9b9e618b
BJ Murphy <crobird@giftbot.org>
2020-12-23 11:28:19 -0800

    Added timing

--- resources/part1.py
+++ resources/part1.py
@@ -9,11 +9,13 @@ import re
 import math
 import collections
 from enum import Enum
+from datetime import datetime, timedelta
 from dataclasses import dataclass
 
 me = os.path.basename(__file__)
 
 DEFAULT_INPUT_FILE = "input/" + me.replace(".py", ".txt")
+STARTED_AT = None
 
 # class Color(Enum):
 #     RED = 0
@@ -25,6 +27,15 @@ class Tile:
     x: int
     y: int
 
+def log_start():
+    global STARTED_AT
+    STARTED_AT = datetime.now()
+    print(f"[{STARTED_AT}]")
+
+def log_end():
+    global STARTED_AT
+    duration = datetime.now() - STARTED_AT
+    print(f"[execution time: {duration}]")
 
 def file_contents(filepath, strip_empty_lines=True):
     empty_lines = not strip_empty_lines
@@ -37,24 +48,25 @@ def parse_file(filepath):
     lines = file_contents(filepath)
     things = []
     for line in lines:
-
-        # Parse with split
-        bits = line.split(',')
-        things.append(Tile(*bits))
-
         # Parse with regex
-        # mobj = re.match(r'', line)
-        # if mobj:
-        #     print(mobj)
+        mobj = re.match(r'', line)
+        if mobj:
+            print(mobj)
 
     return things
 
 
 def main(args):
+    log_start()
+    # ---------
+
     things = parse_file(args.file)
 
     for t in things:
         print(t)
+
+    # ---------
+    log_end()
 
 
 if __name__ == '__main__':
